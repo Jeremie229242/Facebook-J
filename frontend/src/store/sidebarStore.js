b60fa98ddc265d534b79476e8eeab4c86212132c
unknown <jeremie229242@gmail.com>
2026-02-28 19:37:42 +0100

    04 commit

--- frontend/src/store/sidebarStore.js
+++
@@ -1,7 +0,0 @@
-import { create } from 'zustand'
-const useSidebarStore = create((set) =>({
-    isSidebarOpen:false,
-    toggleSidebar: () => set((state) => ({isSidebarOpen: !state.isSidebarOpen}))
-}))
-
-export default useSidebarStore;
